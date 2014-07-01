1c11aeb7035f7969f98b2b0a13a8556d276e469d
Kevin Bellinger <kevinb@outlook.com>
2014-07-01 14:23:38 +0000

    updates to css and session controller for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,12 +5,12 @@ Devise.setup do |config|
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
   config.secret_key = ENV['DEVISE_SECRET_KEY']
-  
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'verify-account@glider.io' 
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
